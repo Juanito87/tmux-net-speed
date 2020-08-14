232264bfe8cfcb551d21f1a434cd30079b6c063b
juanito87 <juancruz.f87@gmail.com>
2020-08-14 14:45:18 -0300

    Cleaning up the code

--- scripts/ip.sh
+++ scripts/ip.sh
@@ -3,14 +3,6 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
-# get_ip()
-# {
-#     local interfaces=$(get_tmux_option @net_speed_interfaces "")
-#     ip=$(ip a s $interfaces | awk -F'[/ ]+' '/inet / {print $3}')
-#     echo -n $ip
-# }
-
-
 main()
 {
     local format=$(get_tmux_option @net_ip_format "IP(%s): %10s")
